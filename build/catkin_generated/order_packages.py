8a85649b14be0e5a95d70a24ba0ddc7a187f7560
awaket <1144013065@qq.com>
2023-02-06 14:11:39 +0800

    注释语句

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ubuntu/pathplanning_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ubuntu/mic_ws/devel;/home/ubuntu/loam_ws/devel;/home/ubuntu/gazebo_test_ws/devel;/home/ubuntu/ucarV2_ws/devel;/home/ubuntu/axg_ws/devel;/opt/ros/melodic'.split(';') if '/home/ubuntu/mic_ws/devel;/home/ubuntu/loam_ws/devel;/home/ubuntu/gazebo_test_ws/devel;/home/ubuntu/ucarV2_ws/devel;/home/ubuntu/axg_ws/devel;/opt/ros/melodic' != '' else []
